b41175f2e0e9412066af927bc66e5a1be63724ff
Eduardo Canales <131183212+educanales@users.noreply.github.com>
2023-10-19 11:12:59 -0300

    Add a name input to Playlist.jsx and edit the styles

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react"
+
 export default function Playlist({ playlist, handleDeleteFromPlaylist }) {
-  
+  const [ playlistName, setPlaylistName ] = useState('Playlist Name');
+  const [ showEdit, setShowEdit ] = useState(false);
+
+  const handleEdit = () => {
+    setShowEdit(!showEdit);
+  }
+
+  const handleNameChange = e => {
+    setPlaylistName(e.target.value);
+  }
+
+
   const list = playlist.map(track =>
     <div className="track-container" key={track.id}>
       <div>
@@ -10,12 +23,23 @@ export default function Playlist({ playlist, handleDeleteFromPlaylist }) {
         <button onClick={() => handleDeleteFromPlaylist(track.id)}>-</button>
       </div>
     </div>
-    )
+    );
 
   return (
     <div className="container">
-      <h2>Playlist</h2>
+      {showEdit ? (
+        <input 
+          className="playlist-name"
+          type="text"
+          value={playlistName}
+          onChange={handleNameChange}
+        />
+      ) : (        
+          <h2 onFocus={handleEdit}>{playlistName}</h2>
+        )
+      }
       {list}
+      <button onClick={handleEdit}>Save to Spotify</button>
     </div>
   )
 }
